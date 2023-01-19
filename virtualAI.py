ef3f79ac134136a3403619a87897fcc250996554
gokul893 <104201541+gokul893@users.noreply.github.com>
2023-01-19 08:37:06 +0530

    Update virtualAI.py

--- virtualAI.py
+++ virtualAI.py
@@ -6,19 +6,24 @@ import pyttsx3
 import operator
 import speech_recognition as sr
 import datetime
-import wikipedia
 import webbrowser
 import os
 import winshell
 import pyjokes
 import ctypes
-import time
 import shutil
 from ecapture import ecapture as ec
 from urllib.request import urlopen
 from requests import get
 import pywhatkit as kit
-import calendar
+import yagmail
+import requests
+from bs4 import BeautifulSoup
+import speedtest
+import pyautogui
+
+from INTRO import play_gif
+play_gif
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -28,7 +33,6 @@ def speak(audio):
 	engine.say(audio)
 	engine.runAndWait()
 
-
 def wishMe():
 	hour = int(datetime.datetime.now().hour)
 	if hour>= 0 and hour<12:
@@ -37,10 +41,9 @@ def wishMe():
 		speak("Good Afternoon Sir !")
 	else:
 		speak("Good Evening Sir !")
-	assname = ("Voice Assisant")
+	assname = ("Voice Assistmant")
 	speak("I am your Assistant")
 	speak(assname)
-
 
 def username():
 	speak("What should i call you sir")
@@ -50,24 +53,24 @@ def username():
 	columns = shutil.get_terminal_size().columns
 	print("Welcome Mr.", uname)
 	speak("How can i Help you, Sir")
-
 	
 def takeCommand():
-	r = sr.Recognizer()
+	recognizer=sr.Recognizer()
 	with sr.Microphone() as source:
+		speak("Listening....")
 		print("Listening...")
-		r.pause_threshold = 1
-		audio = r.listen(source)
+		recognizer.adjust_for_ambient_noise(source,duration=1)
+		recordedaudio=recognizer.listen(source)
 	try:
+		speak("Understanding....")
 		print("Recognizing...")
-		query = r.recognize_google(audio, language ='en-in')
-		print(f"User said: {query}\n")
+		text=recognizer.recognize_google(recordedaudio,language='en-US')
+		speak("your message:{}".format(text))
 	except Exception as e:
 		print(e)
 		print("Unable to Recognize your voice.")
 		return "None"
 	return query
-
 
 if __name__ == '__main__':
 	clear = lambda: os.system('cls')
@@ -76,30 +79,81 @@ if __name__ == '__main__':
 	username()
 	
 	while True:
-		
 		query = takeCommand().lower()
+		if "google" in query:
+			from SearchNow import searchGoogle
+			searchGoogle(query)
 
-		if 'wikipedia' in query:
-			speak('Searching Wikipedia...')
-			query = query.replace("wikipedia", "")
-			results = wikipedia.summary(query, sentences = 3)
-			speak("According to Wikipedia")
-			print(results)
-			speak(results)
+		elif "hello" in query:
+			speak("Hello master   How are you ? ")
 
+		elif "i am fine" in query:
+			speak("That great   Master!")
+
+		elif "how are you" in query:
+			speak("Perfect as always Master")
+
+		elif "Thank you" in query:
+			speak("At your service Master")
+
+		elif "wikipedia" in query:
+			from SearchNow import searchWikipedia
+			searchWikipedia(query)
+
+		elif "youtube" in query:
+			from SearchNow import searchYoutube
+			searchYoutube(query)
+
+		elif "send email" in query:
+			recognizer=sr.Recognizer()
+			with sr.Microphone() as source:
+				speak("Clearing Background noise..")
+				recognizer.adjust_for_ambient_noise(source,duration=1)
+				speak("Waiting for your message...")
+				recordedaudio=recognizer.listen(source)
+				speak("Done Recording..!")
+			try:
+				speak("Printing the message..")
+				text=recognizer.recognize_google(recordedaudio,language='en-US')
+				speak("your message:{}".format(text))
+
+			except Exception as ex:
+				print(ex)
+			reciever='mohang124777@gmail.com'
+			message=text
+			sender=yagmail.SMTP('gokulrajnithyanandhan127@gmail.com')
+			sender.send(to=reciever,subject='This is an automated mail',contents=message)
+			speak("Your email is sent successfully")
+
+		elif "latest news" in query:
+			from NewsRead import latestnews
+			latestnews()
+
+		elif "temperature" in query:
+			search = "temperature in chennai"
+			url = f"https://www.google.com/search?q={search}"
+			r  = requests.get(url)
+			data = BeautifulSoup(r.text,"html.parser")
+			temp = data.find("div", class_ = "BNeawe").text
+			speak(f"current{search} is {temp}")
+
+		elif "weather" in query:
+			search = "temperature in chennai"
+			url = f"https://www.google.com/search?q={search}"
+			r  = requests.get(url)
+			data = BeautifulSoup(r.text,"html.parser")
+			temp = data.find("div", class_ = "BNeawe").text
+			speak(f"current{search} is {temp}")
+
+		elif "play a game" in query:
+			from game import game_play
+			game_play() 
 
 		elif "open facebook" in query:
 			webbrowser.open("www.facebook.com")
 
-		elif"play songs on youtube" in query:
-			kit.playonyt("Kaarkuzhal Kadavaiye")
-
-		elif 'open youtube' in query:
-			speak("\nHere you go to Youtube")
-			webbrowser.open("youtube.com")
-
-		elif 'open google' in query:
-			speak("Sir, what should I search on google")
+		elif 'open chrome' in query:
+			speak("Sir, what should I search on chrome")
 			cm =takeCommand().lower()
 			webbrowser.open(f"{cm}")
 
@@ -109,7 +163,7 @@ if __name__ == '__main__':
 
 		elif 'play music' in query or "play song" in query:
 			speak("Here you go with music")
-			music_dir = "C:\\Music"
+			music_dir = "C:\\Users\\Gokul raj\\Music"
 			songs = os.listdir(music_dir)
 			print(songs)
 			random = os.startfile(os.path.join(music_dir, songs[1]))
@@ -118,42 +172,25 @@ if __name__ == '__main__':
 			strTime = datetime.datetime.now().strftime("%H:%M")
 			speak(f"Sir, the time is {strTime}")
 
+		elif "stop" in query:
+			pyautogui.press("k")
+			speak("video paused")
+
+		elif "play" in query:
+			pyautogui.press("k")
+			speak("video played")
+			
+		elif "full screen" in query:
+			pyautogui.press("f")
+			speak("Full screen enabled")
+
 		elif 'open code' in query:
 			codePath = r"C:\\Users\\Gokul raj\\Microsoft VS Code\\Code.exe"
 			os.startfile(codePath)
-
-		elif 'how are you' in query:
-			speak("I am fine, Thank you")
-			speak("How are you, Sir")
-
-		elif 'fine' in query:
-			speak("It's good to know that your fine")
-
-		elif "what's your name" in query or "What is your name" in query:
-			speak("My Boss call me Mayuri")
-			speak("But my friends call me Mayu")
-
-		elif "do you have friends" in query:
-			speak("Yes I have many friends")
-
-		elif "who is your best friend" in query:
-			speak("I have many friends but for me , Amenda is my best friend")
-
-		elif 'exit' in query:
-			speak("Thanks for giving me your time")
-			exit()
-
-		elif "who made you" in query or "who created you" in query:
-			speak("I have been created by Gokul Raj")
-
-		elif "I love you" in query:
-			speak("ooo no but I have already have a boyfriend")
 	
 		elif 'joke' in query:
+			print(pyjokes.get_joke())
 			speak(pyjokes.get_joke())
-
-		elif "will you be my girlfriend" in query:
-			speak("I'm not sure about, may be you should give me some time")
 
 		elif "ip address" in query:
 			ip= get('https://api.ipify.org').text
@@ -171,10 +208,10 @@ if __name__ == '__main__':
 			print(my_string)
 			def get_operator_fn(op):
 				return {
-					'+' : operator.add, #plus
-					'-' : operator.sub, #minus
-					'x' : operator.mul, #multiplied
-					'divided by' : operator.__truediv__, #divided
+					'+' : operator.add,
+					'-' : operator.sub,
+					'x' : operator.mul,
+					'divided by' : operator.__truediv__,
 				}[op]
 			def eval_binary_expr(op1,oper,op2):
 				op1,op2 = int(op1), int(op2)
@@ -187,29 +224,31 @@ if __name__ == '__main__':
 			query = query.replace("play", "")		
 			webbrowser.open(query)
 
-		elif "who i am" in query:
-			speak("If you talk then definitely your human.")
+		elif "whatsapp" in query:
+			from Whatsapp import sendMessage
+			sendMessage()
 
-		elif "why you came to world" in query:
-			speak("Thanks to the creater. further It's a secret")
-
-		elif 'is love' in query:
-			speak("It is 7th sense that destroy all other senses")
-
-		elif "who are you" in query:
-			speak("I am your virtual assistant created by Gokul Raj")
-
-		elif 'reason for you' in query:
-			speak("I was created as a Minor project by Mister Gokul Raj ")
+		elif "internet speed" in query:
+			wifi  = speedtest.Speedtest()
+			upload_net = wifi.upload()/1048576
+			download_net = wifi.download()/1048576
+			print("Wifi Upload Speed is", upload_net)
+			print("Wifi download speed is ",download_net)
+			speak(f"Wifi download speed is {download_net}")
+			speak(f"Wifi Upload speed is {upload_net}")
 
 		elif 'change background' in query:
 			ctypes.windll.user32.SystemParametersInfoW(20,0,"Location of wallpaper",0)
 			speak("Background changed successfully")
 
+		elif "screenshot" in query:
+			import pyautogui #pip install pyautogui
+			im = pyautogui.screenshot()
+			im.save("ss.jpg")
+
 		elif "what is" in query or "who is" in query:
-			client = wolframalpha.Client("API_ID")
+			client = wolframalpha.Client("WRPK8A-AHKGG5HA9L")
 			res = client.query(query)
-			
 			try:
 				print (next(res.results).text)
 				speak (next(res.results).text)
@@ -220,18 +259,17 @@ if __name__ == '__main__':
 			speak("locking the device")
 			ctypes.windll.user32.LockWorkStation()
 
-		elif 'empty recycle bin' in query:
+		elif 'delete recycle bin' in query:
 			winshell.recycle_bin().empty(confirm = False, show_progress = False, sound = True)
 			speak("Recycle Bin Recycled")
 
-		elif "don't listen" in query or "stop listening" in query:
-			speak("for how much time you want to stop mayuri from listening commands")
-			a = int(takeCommand())
-			time.sleep(a)
-			print(a)
-
-		elif "camera" in query or "take a photo" in query:
-			ec.capture(0, "Voice Assisant Camera ", "img.jpg")
+		elif "camera" in query:
+			pyautogui.press("super")
+			pyautogui.typewrite("camera")
+			pyautogui.press("enter")
+			pyautogui.sleep(2)
+			speak("SMILE")
+			pyautogui.press("enter")
 			
 		elif "write a note" in query:
 			speak("What should i write, sir")
@@ -253,6 +291,24 @@ if __name__ == '__main__':
 			print(file.read())
 			speak(file.read(6))
 
-		elif "open Notepad " in query:
-			npath = "C:\\Windows\\notepad.exe"
-			os.startfile(npath)
+		elif "open" in query:
+			query = query.replace("open","")
+			query = query.replace("jarvis","")
+			pyautogui.press("super")
+			pyautogui.typewrite(query)
+			pyautogui.sleep(2)
+			pyautogui.press("enter")
+
+		elif "volume up" in query:
+			from keyboard import volumeup
+			speak("Turning volume up,sir")
+			volumeup()
+
+		elif "volume down" in query:
+			from keyboard import volumedown
+			speak("Turning volume down, sir")
+			volumedown()
+		
+		elif 'exit' in query:
+			speak("Thanks for giving me your time")
+			exit()
