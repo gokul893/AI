327dde731ce4a6ccc795c5d79b28b5faea58bf70
gokul893 <104201541+gokul893@users.noreply.github.com>
2023-02-16 16:35:50 +0530

    Update virtualAI.py

--- virtualAI.py
+++ virtualAI.py
@@ -29,10 +29,12 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
+#speak function
 def speak(audio):
 	engine.say(audio)
 	engine.runAndWait()
 
+#Wishing the user
 def wishMe():
 	hour = int(datetime.datetime.now().hour)
 	if hour>= 0 and hour<12:
@@ -45,6 +47,7 @@ def wishMe():
 	speak("I am your Assistant")
 	speak(assname)
 
+#getting user name
 def username():
 	speak("What should i call you sir")
 	uname = takeCommand()
@@ -53,19 +56,18 @@ def username():
 	columns = shutil.get_terminal_size().columns
 	print("Welcome Mr.", uname)
 	speak("How can i Help you, Sir")
-	
+
+#Taking commands from user
 def takeCommand():
-	recognizer=sr.Recognizer()
+	r = sr.Recognizer()
 	with sr.Microphone() as source:
-		speak("Listening....")
 		print("Listening...")
-		recognizer.adjust_for_ambient_noise(source,duration=1)
-		recordedaudio=recognizer.listen(source)
+		r.pause_threshold = 1
+		audio = r.listen(source)
 	try:
-		speak("Understanding....")
 		print("Recognizing...")
-		text=recognizer.recognize_google(recordedaudio,language='en-US')
-		speak("your message:{}".format(text))
+		query = r.recognize_google(audio, language ='en-in')
+		print(f"User said: {query}\n")
 	except Exception as e:
 		print(e)
 		print("Unable to Recognize your voice.")
@@ -80,30 +82,38 @@ if __name__ == '__main__':
 	
 	while True:
 		query = takeCommand().lower()
+		#Asking contents in google 
 		if "google" in query:
 			from SearchNow import searchGoogle
 			searchGoogle(query)
-
+		
+		#Greeting1
 		elif "hello" in query:
 			speak("Hello master   How are you ? ")
-
+		
+		#Greeting2
 		elif "i am fine" in query:
 			speak("That great   Master!")
-
+		
+		#Greeting3
 		elif "how are you" in query:
 			speak("Perfect as always Master")
-
+		
+		#Greeting4
 		elif "Thank you" in query:
 			speak("At your service Master")
-
+		
+		#Asking contents in wikipedia
 		elif "wikipedia" in query:
 			from SearchNow import searchWikipedia
 			searchWikipedia(query)
 
+		#Seeing video in youtube
 		elif "youtube" in query:
 			from SearchNow import searchYoutube
 			searchYoutube(query)
 
+		#Sending email using voice
 		elif "send email" in query:
 			recognizer=sr.Recognizer()
 			with sr.Microphone() as source:
@@ -125,10 +135,12 @@ if __name__ == '__main__':
 			sender.send(to=reciever,subject='This is an automated mail',contents=message)
 			speak("Your email is sent successfully")
 
+		#latest news in various category (business,technology,sports,science,entertainment,health)
 		elif "latest news" in query:
 			from NewsRead import latestnews
 			latestnews()
 
+		#Temperature in chennai
 		elif "temperature" in query:
 			search = "temperature in chennai"
 			url = f"https://www.google.com/search?q={search}"
@@ -137,6 +149,7 @@ if __name__ == '__main__':
 			temp = data.find("div", class_ = "BNeawe").text
 			speak(f"current{search} is {temp}")
 
+		#Weather in chennai
 		elif "weather" in query:
 			search = "temperature in chennai"
 			url = f"https://www.google.com/search?q={search}"
@@ -145,22 +158,27 @@ if __name__ == '__main__':
 			temp = data.find("div", class_ = "BNeawe").text
 			speak(f"current{search} is {temp}")
 
+		#Playing a game (stone,paper,scissors)
 		elif "play a game" in query:
 			from game import game_play
 			game_play() 
 
+		#Opening facebook
 		elif "open facebook" in query:
 			webbrowser.open("www.facebook.com")
 
+		#opening chrome
 		elif 'open chrome' in query:
 			speak("Sir, what should I search on chrome")
 			cm =takeCommand().lower()
 			webbrowser.open(f"{cm}")
 
+		#Opening the stack overflow
 		elif 'open stackoverflow' in query or 'overflow' in query:
 			speak("Here you go to Stack Over flow.Happy coding")
 			webbrowser.open("stackoverflow.com")
 
+		#Playing Music in local folder
 		elif 'play music' in query or "play song" in query:
 			speak("Here you go with music")
 			music_dir = "C:\\Users\\Gokul raj\\Music"
@@ -168,35 +186,42 @@ if __name__ == '__main__':
 			print(songs)
 			random = os.startfile(os.path.join(music_dir, songs[1]))
 
+		#telling the time
 		elif 'the time' in query:
 			strTime = datetime.datetime.now().strftime("%H:%M")
 			speak(f"Sir, the time is {strTime}")
 
+		#Stopping the youtube video
 		elif "stop" in query:
 			pyautogui.press("k")
 			speak("video paused")
 
+		#Playing songs in Youtube
 		elif "play" in query:
 			pyautogui.press("k")
 			speak("video played")
-			
-		elif "full screen" in query:
+
+		#Full screen enabled in youtube	
+		elif "full kscreen" in query:
 			pyautogui.press("f")
 			speak("Full screen enabled")
 
+		#Open code
 		elif 'open code' in query:
 			codePath = r"C:\\Users\\Gokul raj\\Microsoft VS Code\\Code.exe"
 			os.startfile(codePath)
-	
+
+		#telling Jokes
 		elif 'joke' in query:
-			print(pyjokes.get_joke())
 			speak(pyjokes.get_joke())
 
+		#Telling your ip address
 		elif "ip address" in query:
 			ip= get('https://api.ipify.org').text
 			print(f"your IP address is {ip}")
 			speak(f"your IP address is {ip}")
 	
+		#voice calculator
 		elif "calculate" in query:
 			r = sr.Recognizer()
 			with sr.Microphone() as source:
@@ -208,10 +233,10 @@ if __name__ == '__main__':
 			print(my_string)
 			def get_operator_fn(op):
 				return {
-					'+' : operator.add,
-					'-' : operator.sub,
-					'x' : operator.mul,
-					'divided by' : operator.__truediv__,
+					'+' : operator.add, #plus
+					'-' : operator.sub, #minus
+					'x' : operator.mul, #multiplied
+					'divided by' : operator.__truediv__, #divided
 				}[op]
 			def eval_binary_expr(op1,oper,op2):
 				op1,op2 = int(op1), int(op2)
@@ -219,15 +244,18 @@ if __name__ == '__main__':
 			speak("Your result is")
 			speak(eval_binary_expr(*(my_string.split())))
 
+		#searching on google:1
 		elif 'search' in query or 'play' in query:
 			query = query.replace("search", "")
 			query = query.replace("play", "")		
 			webbrowser.open(query)
 
+		#opening whatsapp and send message
 		elif "whatsapp" in query:
 			from Whatsapp import sendMessage
 			sendMessage()
 
+		#checking internet speed
 		elif "internet speed" in query:
 			wifi  = speedtest.Speedtest()
 			upload_net = wifi.upload()/1048576
@@ -237,15 +265,18 @@ if __name__ == '__main__':
 			speak(f"Wifi download speed is {download_net}")
 			speak(f"Wifi Upload speed is {upload_net}")
 
+		#changing Background
 		elif 'change background' in query:
 			ctypes.windll.user32.SystemParametersInfoW(20,0,"Location of wallpaper",0)
 			speak("Background changed successfully")
 
+		#screenshoot function
 		elif "screenshot" in query:
-			import pyautogui #pip install pyautogui
+			import pyautogui
 			im = pyautogui.screenshot()
 			im.save("ss.jpg")
 
+		#Searching in google:2
 		elif "what is" in query or "who is" in query:
 			client = wolframalpha.Client("WRPK8A-AHKGG5HA9L")
 			res = client.query(query)
@@ -255,14 +286,17 @@ if __name__ == '__main__':
 			except StopIteration:
 				speak("No results")
 
+		#Lock window
 		elif 'lock window' in query:
 			speak("locking the device")
 			ctypes.windll.user32.LockWorkStation()
 
+		#deleting recycle bin
 		elif 'delete recycle bin' in query:
 			winshell.recycle_bin().empty(confirm = False, show_progress = False, sound = True)
 			speak("Recycle Bin Recycled")
 
+		#Taking a photo
 		elif "camera" in query:
 			pyautogui.press("super")
 			pyautogui.typewrite("camera")
@@ -270,7 +304,8 @@ if __name__ == '__main__':
 			pyautogui.sleep(2)
 			speak("SMILE")
 			pyautogui.press("enter")
-			
+
+		#Creating a note function	
 		elif "write a note" in query:
 			speak("What should i write, sir")
 			note = takeCommand()
@@ -285,12 +320,14 @@ if __name__ == '__main__':
 			else:
 				file.write(note)
 		
+		#showning notes
 		elif "show note" in query:
 			speak("Showing Notes")
 			file = open("jarvis.txt","r")
 			print(file.read())
 			speak(file.read(6))
 
+		#opening any app using pyautogui
 		elif "open" in query:
 			query = query.replace("open","")
 			query = query.replace("jarvis","")
@@ -298,17 +335,27 @@ if __name__ == '__main__':
 			pyautogui.typewrite(query)
 			pyautogui.sleep(2)
 			pyautogui.press("enter")
+		
+		#translate function
+		elif "translate" in query:
+			from Translator import translategl
+			query = query.replace("jarvis","")
+			query = query.replace("translate","")
+			translategl(query)
 
+		#Volume down function
 		elif "volume up" in query:
 			from keyboard import volumeup
 			speak("Turning volume up,sir")
 			volumeup()
 
+		#Volume down function
 		elif "volume down" in query:
 			from keyboard import volumedown
 			speak("Turning volume down, sir")
 			volumedown()
 		
+		#closing the program
 		elif 'exit' in query:
 			speak("Thanks for giving me your time")
 			exit()
